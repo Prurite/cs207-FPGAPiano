83d6b15c80773b32866453fed8997d370c9c0fd4
Yazawa Nico <hzh3128@163.com>
2024-01-04 15:50:47 +0800

    fixed score history

--- pageScoreHistory.sv
+++ pageScoreHistory.sv
@@ -7,10 +7,10 @@ module pageScoreHistory(
     output byte read_record_id,
     input PlayRecord record_data
 );
-    localparam UP = 4'b1000;
-    localparam DOWN = 4'b0100;
-    localparam LEFT = 4'b0010;
-    localparam RIGHT = 4'b0001;
+    localparam UP       = 4'b1000;
+    localparam DOWN     = 4'b0100;
+    localparam LEFT     = 4'b0010;
+    localparam RIGHT    = 4'b0001;
 
     ScreenText text;
     SegDisplayText seg;
@@ -33,17 +33,17 @@ module pageScoreHistory(
             // Initialize text
             text[0] <=  "======    Score  History    ======";
             text[1] <=  "                                  ";
-            text[2] <=  " 1| User1 | Tiny Stars      | 4487";
-            text[3] <=  " 2|       |                 |     ";
-            text[4] <=  " 3|       |                 |     ";
-            text[5] <=  " 4|       |                 |     ";
-            text[6] <=  " 5|       |                 |     ";
-            text[7] <=  " 6|       |                 |     ";
-            text[8] <=  " 7|       |                 |     ";
-            text[9] <=  " 8|       |                 |     ";
-            text[10] <= " 9|       |                 |     ";
-            text[11] <= "                                  ";
-            text[12] <= " [<] Back                         ";
+            text[2] <=  " 1|User1 | Tiny Stars     | 4487";
+            text[3] <=  " 2|      |                |     ";
+            text[4] <=  " 3|      |                |     ";
+            text[5] <=  " 4|      |                |     ";
+            text[6] <=  " 5|      |                |     ";
+            text[7] <=  " 6|      |                |     ";
+            text[8] <=  " 7|      |                |     ";
+            text[9] <=  " 8|      |                |     ";
+            text[10] <= " 9|      |                |     ";
+            text[11] <= "                                ";
+            text[12] <= " [<] Back                       ";
         end
         else begin
             case (user_in.arrow_keys)
@@ -61,6 +61,40 @@ module pageScoreHistory(
                     text[i][8:15] <= mstr;
                 else
                     text[i][8:15] <= "0" + i;
+        end
+    end
+
+    displayHisLine d1(.clk(prog_clk), .rst(rst), .rec_id(1), .line(text[2]));
+    displayHisLine d2(.clk(prog_clk), .rst(rst), .rec_id(2), .line(text[3]));
+    displayHisLine d3(.clk(prog_clk), .rst(rst), .rec_id(3), .line(text[4]));
+    displayHisLine d4(.clk(prog_clk), .rst(rst), .rec_id(4), .line(text[5]));
+    displayHisLine d5(.clk(prog_clk), .rst(rst), .rec_id(5), .line(text[6]));
+    displayHisLine d6(.clk(prog_clk), .rst(rst), .rec_id(6), .line(text[7]));
+    displayHisLine d7(.clk(prog_clk), .rst(rst), .rec_id(7), .line(text[8]));
+    displayHisLine d8(.clk(prog_clk), .rst(rst), .rec_id(8), .line(text[9]));
+    displayHisLine d9(.clk(prog_clk), .rst(rst), .rec_id(9), .line(text[10]));
+endmodule
+
+module displayHisLine(
+    input logic clk, rst,
+    input byte rec_id,
+    output [0:32*8-1] line
+);
+    PlayRecord rec;
+    bit [39:0] uid_raw, score_raw;
+    RecordStorageManager rec_m(.clk(clk), .sys_rst(rst), .read_record_id(rec_id), .write_record_id(0), .current_record_data(rec));
+    binary2Str b2(.intx(rec.score), .out_str(score_raw));
+    binary2Str b3(.intx(rec.user_id), .out_str(uid_raw));
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            line[2*8:3*8-1] <= "|";
+            line[9*8:10*8-1] <= "|";
+            line[26*8:27*8-1] <= "|";
+        end
+        else begin
+            line[3*8:8*8-1] <= {"User", uid_raw[7:0]};
+            line[10*8:26*8-1] <= rec.name;
+            line[27*8:32*8-1] <= score_raw;
         end
     end
 endmodule
